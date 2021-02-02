3bb95ffae5657e422bb6b3fbf2b7933ac4efc3ad
Joel <j_90@live.co.uk>
2021-02-02 20:14:28 +0000

    alters projects section

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+// myID = document.getElementsByClassName("projects__card__right");
+
+// var myScrollFunc = function () {
+//   var y = window.scrollY;
+//   if (y >= 800) {
+//     myID.className = "bottomMenu show";
+//   } else {
+//     myID.className = "bottomMenu hide";
+//   }
+// };
+
+// window.addEventListener("scroll", myScrollFunc);
