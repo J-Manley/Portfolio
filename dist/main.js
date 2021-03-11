3781e10b69692fb499b5ca4e9a8aae4e90abc5aa
Joel <j_90@live.co.uk>
2021-03-11 06:18:03 +0000

    adds js to projects section

--- dist/main.js
+++ dist/main.js
@@ -7,45 +7,48 @@
 //   };
 // }
 
-function debounce(func, wait = 20, immediate = true) {
-  var timeout;
-  return function () {
-    var context = this,
-      args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
+// const slideCardUp = document.querySelectorAll(".slide-in");
 
-const slideCardUp = document.querySelectorAll(".projects__cards");
+// // Check all of the images and slide them in or out as needed
+// function checkSlide(e) {
+//   // What part of the content is visible in the window?
+//   const windowTop = window.scrollY;
+//   const windowBottom = window.scrollY + window.innerHeight;
 
-// Check all of the images and slide them in or out as needed
-function checkSlide(e) {
-  // What part of the content is visible in the window?
-  const windowTop = window.scrollY;
-  const windowBottom = window.scrollY + window.innerHeight;
+//   slideCardUp.forEach((card) => {
+//     const cardTop = card.offsetTop;
+//     const cardBottom = card.offsetTop + card.height;
+//     const cardMiddle = (cardTop + cardBottom) / 2;
 
-  slideCardUp.forEach((card) => {
-    const cardTop = card.offsetTop;
-    const cardBottom = card.offsetTop + card.height;
-    const cardMiddle = (cardTop + cardBottom) / 2;
+//     // The image should slide in if its middle is visible in the window
+//     if (cardMiddle >= windowTop && cardMiddle <= windowBottom) {
+//       card.classList.add("active");
+//     }
 
-    // The image should slide in if its middle is visible in the window
-    if (cardMiddle >= windowTop && cardMiddle <= windowBottom) {
-      card.classList.add("active");
-    }
+//     // The image should slide out if entirely above or below visible window
+//     if (cardBottom < windowTop || cardTop > windowBottom) {
+//       card.classList.remove("active");
+//     }
+//   });
+// }
 
-    // The image should slide out if entirely above or below visible window
-    if (cardBottom < windowTop || cardTop > windowBottom) {
-      card.classList.remove("active");
+// window.addEventListener("scroll", debounce(checkSlide));
+
+const projects = document.querySelectorAll(".fade-in");
+
+const appearOptions = {};
+
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
     }
   });
-}
+}, appearOptions);
 
-window.addEventListener("scroll", debounce(checkSlide));
+projects.forEach((project) => {
+  appearOnScroll.observe(project);
+});
