928b246fdb52c28e71f36a3dea648850e1e4f0bc
Joel <j_90@live.co.uk>
2021-04-29 17:18:15 +0100

    modifies mobile layout of nav section

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,7 @@ const appearOptions = {
   rootMargin: "0px 0px -350px 0px",
 };
 
-const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
